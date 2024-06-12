a2e322a6ffe473efa7422985710b5ee1721f8b6f
Christy61 <2787449768@qq.com>
2024-06-12 21:54:27 +0800

    fix

---
+++ static/js/bottom.js
@@ -0,0 +1,9 @@
+function showContent(section) {
+    const sections = ['Abstract', 'Statistics-and-Distribution', 'Novel-View-Synthesis', '3D-Shape-Reconstruction', 'References'];
+    sections.forEach(sec => {
+        document.getElementById(`content-${sec}`).classList.remove('visible');
+        document.querySelector(`.button-card[onclick="showContent('${sec}')"]`).classList.remove('active');
+    });
+    document.getElementById(`content-${section}`).classList.add('visible');
+    document.querySelector(`.button-card[onclick="showContent('${section}')"]`).classList.add('active');
+}
