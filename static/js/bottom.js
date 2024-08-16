3baaeca0ef848e0cd2b75b47f1327343f175ad63
Christy61 <2787449768@qq.com>
2024-08-17 00:16:57 +0800

    add web scoreboard

--- static/js/bottom.js
+++ static/js/bottom.js
@@ -1,5 +1,5 @@
 function showContent(section) {
-    const sections = ['Preview', 'Statistics-and-Distribution', 'Novel-View-Synthesis', '3D-Shape-Reconstruction', 'References'];
+    const sections = ['Preview', 'Statistics-and-Distribution', 'Web-Scoreboard', 'Novel-View-Synthesis', '3D-Shape-Reconstruction', 'References'];
     sections.forEach(sec => {
         document.getElementById(`content-${sec}`).classList.remove('visible');
         document.querySelector(`.button-card[onclick="showContent('${sec}')"]`).classList.remove('active');
